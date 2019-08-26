671537fb981b11dff51e4fd405900976a62b8351
Dragos <dragos199993@gmail.com>
2019-08-26 22:06:09 +0300

    Init.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const WidgetInstance = (window as any).ReactWidget.default;
 
   useEffect(() => {
-    const WidgetInstance = (window as any).ReactWidget.default;
     WidgetInstance.new({ selector: '.react-widget-container' }).render();
   },[]);
   window.addEventListener('init', (res) => console.log("INITIALIZED", res));
   window.addEventListener('clickWidget', (res) => console.log("CLICKED WIDGET", res));
+  window.addEventListener('destroy', (res) => console.log("WIDGET DESTROYED", res));
+
+  // DESTROY WIDGET FROM HERE TO TEST DESTROY WIDGET
+  // setTimeout(() => {
+  //   WidgetInstance.unmount({ selector: '.react-widget-container' });
+  // }, 3000);
   return (
     <div className="App">
       <header className="App-header">
